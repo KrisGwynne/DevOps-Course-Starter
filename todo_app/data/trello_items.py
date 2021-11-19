a6ffe9db94b94663de887c59d784f376acb0dec5
Kris <kris.gwynne@softwire.com>
2021-11-19 12:27:20 +0000

    feat: Add trello add item method

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -11,4 +11,15 @@ def get_items():
     # TODO: Handle bad api response
     res = requests.get(f'{_BASE_URL}/1/boards/{os.getenv("BOARD_ID")}/lists', params=params)
     return [Card(card, list['name']) for list in res.json() for card in list['cards']]
+
+def add_item(title):
+    # Get the To-Do list id
+    # TODO: handle api failure
+    res = requests.get(f'{_BASE_URL}/1/boards/{os.getenv("BOARD_ID")}/lists', params=_API_QUERY_PARAMS)
+    list_id = next(list for list in res.json() if list['name'] == "To Do")["id"]
+    
+    # Add card to the To-Do list
+    params = _API_QUERY_PARAMS | {'name': title, 'idList': list_id}
+    # TODO: handle api failure
+    requests.post(f'{_BASE_URL}/1/cards', params=params)
     
