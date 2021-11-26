12c3ba7ce9a0d6c1c637a105844b263a24e34152
Kris <kris.gwynne@softwire.com>
2021-11-26 13:02:06 +0000

    fix: handle api errors

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
+from flask.helpers import flash, make_response
 import  todo_app.data.session_items as session
 import todo_app.data.trello_items as trello
 from todo_app.flask_config import Config
+from todo_app.models.ApiException import ApiException
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -9,22 +11,42 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = trello.get_items()
+    items = []
+    try:
+        items = trello.get_items()
+    except ApiException as err:
+        flash(err.message, "error")
+
     sorted_items = sorted(items, key=lambda item: item.status, reverse=True)
     return render_template("index.html", items=sorted_items)
 
 @app.route('/item', methods=['POST'])
 def add_item():
     title = request.form.get("item_title")
-    trello.add_item(title)
+    try:
+        trello.add_item(title)
+        flash("Added new to-do item", "success")
+    except ApiException as err:
+        flash(err.message, "error")
+
     return redirect("/")
 
 @app.route('/items/<id>', methods=['POST'])
 def update_item(id):
-    trello.complete_item(id)
+    try:
+        trello.complete_item(id)
+        flash("Completed to-do item", "success")
+    except ApiException as err:
+        flash(err.message, "error")
+
     return redirect('/')
 
 @app.route('/items/delete/<id>', methods=['POST'])
 def delete_item(id):
-    session.remove_item(id)
+    try:
+        session.remove_item(id)
+        flash("Deleted to-do item", "success")
+    except ApiException as err:
+        flash(err.message, "error")
+
     return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from todo_app.data.Card import Card
+from todo_app.models.ApiException import ApiException
+from todo_app.models.Card import Card
 
 _BASE_URL = "https://api.trello.com"
 
@@ -8,8 +9,11 @@ _API_QUERY_PARAMS = {'key': os.getenv("API_KEY"), 'token': os.getenv("API_TOKEN"
 
 def get_items():
     params = _API_QUERY_PARAMS | {'cards': 'open'}
-    # TODO: Handle bad api response
     res = requests.get(f'{_BASE_URL}/1/boards/{os.getenv("BOARD_ID")}/lists', params=params)
+
+    if not res.ok:
+        raise ApiException("Error getting to-do items")
+
     return [Card.from_trello_card(card, list) for list in res.json() for card in list['cards']]
 
 def add_item(title):
@@ -18,8 +22,10 @@ def add_item(title):
     
     # Add card to the To-Do list
     params = _API_QUERY_PARAMS | {'name': title, 'idList': list_id}
-    # TODO: handle api failure
-    requests.post(f'{_BASE_URL}/1/cards', params=params)
+    res = requests.post(f'{_BASE_URL}/1/cards', params=params)
+
+    if not res.ok:
+        raise ApiException("Error adding a new to-do item")
 
 def complete_item(id):
     # Get the Done list id
@@ -27,10 +33,16 @@ def complete_item(id):
 
     # Move card to Done list
     params = _API_QUERY_PARAMS | {'idList': list_id}
-    requests.put(f'{_BASE_URL}/1/cards/{id}', params=params)
+    res = requests.put(f'{_BASE_URL}/1/cards/{id}', params=params)
+
+    if not res.ok:
+        raise ApiException("Error completing to-do item")
 
 
 def get_list_id(list_name):
-    # TODO: handle api failure
     res = requests.get(f'{_BASE_URL}/1/boards/{os.getenv("BOARD_ID")}/lists', params=_API_QUERY_PARAMS)
+
+    if not res.ok:
+        raise ApiException("Error getting to-do items")
+
     return next(list for list in res.json() if list['name'] == list_name)["id"]
