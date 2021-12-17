2f51c9d436f855b0f3f2386ce04e899c6d535ac8
Kris <kris.gwynne@softwire.com>
2021-12-17 13:27:58 +0000

    test: add integration test for index route

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,60 +5,63 @@ from todo_app.flask_config import Config
 from todo_app.models.ApiException import ApiException
 from todo_app.view_models.item_view_model import ItemViewModel
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
 
-@app.route('/')
-def index():
-    items = []
-    try:
-        items = trello.get_items()
-    except ApiException as err:
-        flash(err.message, "error")
+    @app.route('/')
+    def index():
+        items = []
+        try:
+            items = trello.get_items()
+        except ApiException as err:
+            flash(err.message, "error")
 
-    sorted_items = sorted(items, key=lambda item: item.status, reverse=True)
-    item_view_model = ItemViewModel(sorted_items)
-    return render_template("index.html", view_model=item_view_model)
+        sorted_items = sorted(items, key=lambda item: item.status, reverse=True)
+        item_view_model = ItemViewModel(sorted_items)
+        return render_template("index.html", view_model=item_view_model)
 
-@app.route('/item', methods=['POST'])
-def add_item():
-    title = request.form.get("item_title")
-    try:
-        trello.add_item(title)
-        flash("Added new to-do item", "success")
-    except ApiException as err:
-        flash(err.message, "error")
+    @app.route('/item', methods=['POST'])
+    def add_item():
+        title = request.form.get("item_title")
+        try:
+            trello.add_item(title)
+            flash("Added new to-do item", "success")
+        except ApiException as err:
+            flash(err.message, "error")
 
-    return redirect("/")
-    return render_template("index.html", view_model=item_view_model)
+        return redirect("/")
+        return render_template("index.html", view_model=item_view_model)
 
-@app.route('/items/<id>/start', methods=['POST'])
-def start_item(id):
-    try:
-        trello.start_item(id)
-        flash("STarted to-do item", "success")
-    except ApiException as err:
-        flash(err.message, "error")
+    @app.route('/items/<id>/start', methods=['POST'])
+    def start_item(id):
+        try:
+            trello.start_item(id)
+            flash("STarted to-do item", "success")
+        except ApiException as err:
+            flash(err.message, "error")
 
-    return redirect('/')
+        return redirect('/')
 
-@app.route('/items/<id>/complete', methods=['POST'])
-def complete_item(id):
-    try:
-        trello.complete_item(id)
-        flash("Completed to-do item", "success")
-    except ApiException as err:
-        flash(err.message, "error")
+    @app.route('/items/<id>/complete', methods=['POST'])
+    def complete_item(id):
+        try:
+            trello.complete_item(id)
+            flash("Completed to-do item", "success")
+        except ApiException as err:
+            flash(err.message, "error")
 
-    return redirect('/')
+        return redirect('/')
 
-@app.route('/items/delete/<id>', methods=['POST'])
-def delete_item(id):
-    try:
-        trello.delete_item(id)
-        flash("Deleted to-do item", "success")
-    except ApiException as err:
-        flash(err.message, "error")
+    @app.route('/items/delete/<id>', methods=['POST'])
+    def delete_item(id):
+        try:
+            trello.delete_item(id)
+            flash("Deleted to-do item", "success")
+        except ApiException as err:
+            flash(err.message, "error")
 
-    return redirect('/')
+        return redirect('/')
+    
+    return app
