20c4277e4df801dd22a0eb393ad68e0fa7987247
Kris Gwynne <Kris.Gwynne@softwire.com>
2022-05-13 17:03:55 +0100

    Update Dockerfile with test target

--- e2e_tests/e2e_test.py
+++ e2e_tests/e2e_test.py
@@ -1,17 +1,16 @@
-from setup import driver, app_with_temp_board
+from .setup import driver, app_with_temp_board
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 class TestE2e:
     @staticmethod
-    def test_app_title(driver, app_with_temp_board):
+    def test_app_title(driver: webdriver.Firefox, app_with_temp_board):
         driver.get('http://localhost:5000/')
         assert driver.title == 'To-Do App'
 
     @staticmethod
     def test_create_new_todo_item(driver: webdriver.Firefox, app_with_temp_board):
-        print('starting test')
         driver.get('http://localhost:5000/')
         elem = driver.find_element(By.NAME, 'item_title')
         elem.clear()
