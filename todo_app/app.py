c70ec5198402ccce432cb8d761b2d24d53b34185
Kris Gwynne <Kris.Gwynne@softwire.com>
2022-08-19 17:27:38 +0100

    fix: remove refrences to trello

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, redirect, render_template, request
 from flask.helpers import flash
 from todo_app.data.item_service import ItemService
-import todo_app.data.trello_items as trello
 from todo_app.flask_config import Config
 from todo_app.models.ApiException import ApiException
 from todo_app.view_models.item_view_model import ItemViewModel
--- todo_app/models/Card.py
+++ todo_app/models/Card.py
@@ -5,9 +5,5 @@ class Card:
         self.status = status
 
     @classmethod
-    def from_trello_card(cls, card, list):
-        return cls(card['id'], card['name'], list['name'])
-
-    @classmethod
     def from_database(cls, item):
         return cls(item['_id'], item['title'], item['status'])
