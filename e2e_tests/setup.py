9a42afa66d8dafc6d99a1b7922f21edebe1767f9
Kris Gwynne <Kris.Gwynne@softwire.com>
2022-08-19 16:58:48 +0100

    fix: fix broken e2e and integration tests

--- e2e_tests/setup.py
+++ e2e_tests/setup.py
@@ -1,5 +1,4 @@
 from time import sleep
-import requests
 import os
 import pytest
 from threading import Thread
@@ -7,30 +6,19 @@ from todo_app import app
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 from dotenv import load_dotenv, find_dotenv
+import pymongo
 
-def create_e2e_trello_board():
-    params = {'key': os.environ.get("API_KEY"), 'token': os.environ.get("API_TOKEN")}
-    board_name = 'e2e_board'
-    organisation_id = os.environ.get("ORGANISATION_ID")
-    params.update({'name': board_name, 'idOrganization': organisation_id})
-    requests.post('https://api.trello.com/1/boards', params=params)
-
-    _API_QUERY_PARAMS = {'key': os.environ.get("API_KEY"), 'token': os.environ.get("API_TOKEN")}
-    res = requests.get(f'https://api.trello.com/1/organizations/{organisation_id}/boards', params=_API_QUERY_PARAMS)
-
-    return next(board['id'] for board in res.json() if board['name'] == board_name)
-
-def delete_e2e_trello_board(id):
-    _API_QUERY_PARAMS = {'key': os.environ.get("API_KEY"), 'token': os.environ.get("API_TOKEN")}
-    requests.delete(f'https://api.trello.com/1/boards/{id}', params=_API_QUERY_PARAMS)
+def drop_e2e_database(database_name):
+    client = pymongo.MongoClient(os.getenv('CONNECTION_STRING'))
+    client.drop_database(database_name)
 
 @pytest.fixture(scope='module')
 def app_with_temp_board():
     file_path = find_dotenv('.env')
     load_dotenv(file_path, override=True)
-    # Create the new board & update the board id environment variable
-    board_id = create_e2e_trello_board()
-    os.environ['BOARD_ID'] = board_id
+    # Mongo will create a new db for e2e tests when creating new documents
+    e2e_database_name = 'todo-app-e2e'
+    os.environ['DATABASE_NAME'] = e2e_database_name
     # construct the new application
     application = app.create_app()
     # start the app in its own thread.
@@ -42,7 +30,7 @@ def app_with_temp_board():
     yield application
     # Tear Down
     thread.join(1)
-    delete_e2e_trello_board(board_id)
+    drop_e2e_database(e2e_database_name)
 
 @pytest.fixture(scope="module")
 def driver():
